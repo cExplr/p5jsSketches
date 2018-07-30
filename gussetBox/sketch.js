b498511fc3050d9caa398e7d04798145550ffe4a
cexplr <code.explore.95@gmail.com>
2018-07-30 14:27:42 +0800

    Added new function to findClosestVertex to the clicked area and draw an ellipse for the point on the Grid.gridVertex

--- gussetBox/sketch.js
+++ gussetBox/sketch.js
@@ -1,9 +1,9 @@
 var Paper = {
 	//THIS PROGRAM ASSUMES THAT A SQUARE PAPER IS USED
 	//IF YOU WANT NON SQUARE GRID THEN REIMPLEMENT THE DRAW GRID
-
-	paperLength:400.0
+	paperLength:400
 };
+
 
 var Grid={
 	gridDivisionNumber:0,//change this
@@ -12,6 +12,26 @@ var Grid={
 	divisionNoX:0, //measured in pixels\
 	divisionNoY:0
 };
+
+//Created a vertex "class"
+function Vertex(){
+	this.x=0;
+	this.y=0;
+};
+Vertex.prototype.setX=function(x){
+	this.x = x;
+}
+Vertex.prototype.setY=function(y){
+	this.y=y;
+}
+
+Vertex.prototype.getX=function(){
+	return this.x;
+}
+Vertex.prototype.getY=function(){
+	return this.y;
+}
+
 
 function setup(){
 	Grid.gridDivisionNumber = 16;	//Draw 16 grids
@@ -63,4 +83,64 @@ function drawGrid(gridDivisionNumber){
 	// 	console.log("( "+ Grid.gridVertexX[i] + "," + Grid.gridVertexY[i] +" )");
 	// }
 	console.log("Done initializing all grid vertex");
+}
+
+function mouseClicked(){
+	if(mouseButton==LEFT){ //if normal left mouseClick
+		// console.log("Left Clicked");
+		var nearestVertex = new Vertex();
+		nearestVertex = searchForClosestVertex();
+		
+		if (!(nearestVertex.getX() < 0 || nearestVertex.y <0)){
+			//SELECT THE VERTEX nearestVertex and draw a point on it
+			//Store this vertex into selectedVertexArray
+			strokeWeight(1);
+			fill(0,0,255);
+			ellipse(nearestVertex.getX(),nearestVertex.getY(),8);
+		}
+	}
+}
+
+function searchForClosestVertex(){
+
+	var clickPositionX = pmouseX;
+	var clickPositionY = pmouseY;
+
+	var plusMinus = 6/100;
+
+	for (var x = 0 ; x < Grid.gridVertexX.length;x++){
+		if(clickPositionX >= (Grid.gridVertexX[x] - Grid.gridVertexX[x]*plusMinus) && clickPositionX <= (Grid.gridVertexX[x] + Grid.gridVertexX[x]*plusMinus)){
+			
+			for (var y = 0 ; y < Grid.gridVertexY.length;y++){
+				if (clickPositionY >= (Grid.gridVertexY[y] - Grid.gridVertexY[y] * plusMinus) &&clickPositionY <= (Grid.gridVertexY[y] + Grid.gridVertexY[y] * plusMinus) ) {
+					
+					var closestVertexToClick = new Vertex();
+					closestVertexToClick.setX(Grid.gridVertexX[x]);
+					closestVertexToClick.setY(Grid.gridVertexY[y]);
+					// console.log("Clicked within range for X at : "+ pmouseX + " where grid vertex falls on " + Grid.gridVertexX[x]);
+					// console.log("Clicked within range for y : " + pmouseY + " where grid vertex falls on " + Grid.gridVertexY[y]);
+					console.log("closestVertexToClick : "+ closestVertexToClick.getX()+","+closestVertexToClick.getY());
+					return closestVertexToClick;
+				}
+			}
+		}
+	}
+
+	var closestVertexToClick = new Vertex();
+	closestVertexToClick.setX(-1);
+	closestVertexToClick.setY(-1);
+	
+	return closestVertexToClick;
+
+}
+
+function draw(){
+	if(keyIsPressed){
+		reset();
+
+	}
+}
+
+function reset(){
+	console.log("Reset!");
 }
